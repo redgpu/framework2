34265cb36bca9aecb8b9c593714845a0ade1e6af
Constantine Tarasenkov <iamvfx@gmail.com>
2023-11-14 12:24:08 +0300

    Add more examples

---
+++ shader_custom.frag.glsl
@@ -0,0 +1,25 @@
+uniform sampler2D tColor;
+uniform sampler2D tDepth;
+uniform vec2 colorDimensions;
+uniform vec2 writeDimensions;
+uniform float time;
+uniform float parameter0;
+uniform float parameter1;
+uniform float parameter2;
+uniform float parameter3;
+uniform float parameter4;
+uniform float parameter5;
+uniform float parameter6;
+uniform float parameter7;
+uniform float parameter8;
+uniform float parameter9;
+uniform float parameter10;
+uniform float parameter11;
+uniform float parameter12;
+uniform float parameter13;
+uniform float parameter14;
+uniform float parameter15;
+
+void main() {
+  gl_FragColor = vec4(1.0, 0.0, 1.0, 1.0);
+}
